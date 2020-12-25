57e1f0122acb8f8759290f72b991fd3b4a380440
Ronuk Raval <ronuk.raval@gmail.com>
2020-12-25 13:35:57 -0500

    Refactor to NixOS module
    Expose high level configuration declarations that implement the lower
    level definitions.

--- default.nix
+++ default.nix
@@ -1,7 +1,6 @@
 # The canonical NixOS machine for rraval.
 #
 # Setup to use UEFI with a single LUKS encrypted disk with LVM on the inside.
-# Filesystem options are tuned for SSDs.
 #
 # TODO: Turn this into a nix flake.
 #
@@ -10,172 +9,244 @@
 # TODO: XFCE stop session autosave. Fully manage session configuration. XFCE start xmonad.
 #
 # TODO: Integrate all other dotfiles.
-{
-  # string: The `nixos-option networking.hostname` for this machine.
-  hostName,
-
-  # string: The SHA256 hash of the login password. Separated out to prevent it
-  # being checked into Git.
-  rravalHashedPassword,
-
-  # path: Something like `/dev/disk/by-uuid/...` that points to the encrypted
-  # disk device.
-  encryptedDisk,
-
-  # The `<nixpkgs>` set to work off. Should be `nixos-unstable`.
-  pkgs,
-  config,
-}: let
+{ pkgs, lib, config, ... }: let
+  inherit (lib)
+    mkOption
+    mkEnableOption
+    mkIf
+    mkMerge
+    types
+  ;
   util = import ./util.nix;
-  userCfg = config.home-manager.users.rraval;
+  cfg = config.rravalBox;
 in {
-  nixpkgs.config.allowUnfree = true;
-  networking.hostName = hostName;
-  time.timeZone = "America/Toronto";
-  system.stateVersion = "20.09";
-  nix.allowedUsers = [ "rraval" ];
+  options.rravalBox = {
+    enable = mkEnableOption "Configure this machine for rraval";
 
-  boot = {
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-    };
-    initrd.luks.devices = {
-      decrypted0 = {
-        device = (
-          assert builtins.pathExists encryptedDisk;
-          builtins.toString encryptedDisk
-        );
-        preLVM = true;
-        allowDiscards = true;
-      };
-    };
-  };
-
-  # SSD tuning
-  fileSystems."/".options = [ "noatime" "nodiratime" "discard" ];
-
-  # global useDHCP is deprecated
-  networking.useDHCP = false;
-
-  i18n.defaultLocale = "en_CA.UTF-8";
-  console.useXkbConfig = true;
-
-  users = {
-    mutableUsers = false;
-
-    groups.rraval = {
-      gid = 1000;
-    };
-
-    users.rraval = {
-      isNormalUser = true;
-      uid = 1000;
-      group = "rraval";
-      extraGroups = [ "wheel" ];
-      hashedPassword = rravalHashedPassword;
-      createHome = true;
-      home = "/home/rraval";
-      shell = pkgs.fish;
-    };
-  };
-
-  services.xserver = {
-    enable = true;
-    xkbOptions = "ctrl:nocaps";
-    displayManager.defaultSession = "xfce";
-    desktopManager.xfce = {
-      enable = true;
-      enableXfwm = false;
-    };
-  };
-
-  environment = {
-    systemPackages = with pkgs; [
-      fish
-      git
-      neovim
-    ];
-
-    variables = {
-      EDITOR = "nvim";
-      VISUAL = "nvim";
-    };
-  };
-
-  home-manager = {
-    useUserPackages = true;
-    useGlobalPkgs = true;
-
-    users.rraval = {
-      programs = {
-        firefox = {
-          enable = true;
-        };
-
-        fish = {
-          enable = true;
-        };
-
-        git = {
-          enable = true;
-          userName = "Ronuk Raval";
-          userEmail = "ronuk.raval@gmail.com";
-        };
-
-        neovim = {
-          enable = true;
-          extraConfig = ''
-            " mandatory to get plugins to generate
-          '';
-          plugins = with pkgs.vimPlugins; [
-            vim-nix
-          ];
-        };
+    system = {
+      locale = mkOption {
+        default = "en_CA.UTF-8";
+        description = "System locale";
+        type = types.str;
       };
 
-      xsession = {
-        enable = true;
+      timeZone = mkOption {
+        default = "America/Toronto";
+        description = "System timezone";
+        type = types.str;
+      };
+    };
 
-        # There's some spooky action at a distance here.
-        #
-        # The login manager is going to launch the script at `~/.xsession`.
-        #
-        # We want `~/.xsession` to launch `xfce4-session`, which in turn
-        # launches `xfdesktop`, `xmonad`, `xfce4-panel` etc.
-        #
-        # However, we also want to use the home-manager xmonad derivation
-        # instead of writing our own, since it wraps up the installation in
-        # nice configuration options and sets up recompilation when `xmonad.hs`
-        # changes on reconciliation.
-        # https://github.com/nix-community/home-manager/blob/master/modules/services/window-managers/xmonad.nix
-        #
-        # The home-manager xmonad derivation explicitly sets up
-        # `windowManager.command`, which is what is launched by `.xsession`, so
-        # we have to juggle things to get the configuration we want.
+    user = {
+      name = mkOption {
+        description = "Username in the single-user system";
+        type = types.str;
+      };
 
-        initExtra = let
-          xmonad = util.findPackage userCfg.home.packages "xmonad-with-packages";
-        in ''
-          # We only really need to run this once.
-          xfconf-query -c xfce4-session -p /general/SaveOnExit -s false
+      sha256Password = mkOption {
+        description = "SHA 256 hash of user in single-user system. Use `mkpasswd -m sha-512` to generate";
+        type = types.str;
+      };
 
-          # ${xmonad}/bin/xmonad
+      realName = mkOption {
+        description = "Full name to use in Git commits etc.";
+        type = types.str;
+      };
+
+      email = mkOption {
+        description = "Email to use in Git commits etc.";
+        type = types.str;
+      };
+    };
+
+    networking = {
+      hostName = mkOption {
+        description = "Networking hostname of this machine";
+        type = types.str;
+      };
+
+      wiredEthernet = mkOption {
+        description = "Optional single network interface to bring up with DHCP";
+        type = types.nullOr types.str;
+      };
+    };
+
+    rootDevice = {
+      encryptedDisk = mkOption {
+        description = ''
+          Single block device to mount as root.
+
+          This box configuration assumes a single large partition, encrypted
+          with LUKS, with LVM on the inside.
         '';
+        type = types.path;
+      };
 
-        # Override (with `mkForce`) to make `~/.xsession` launch xfce4-session
-        # instead of xmonad directly
-        windowManager.command = pkgs.lib.mkForce "${pkgs.runtimeShell} ${pkgs.xfce.xfce4-session.xinitrc}";
-
-        windowManager.xmonad = {
-          enable = true;
-          enableContribAndExtras = true;
-          extraPackages = haskellPkgs: with haskellPkgs; [
-            dbus
-          ];
-          config = ./xmonad.hs;
-        };
+      isSolidState = mkOption {
+        description = "Is the block device backed by a solid state drive?";
+        type = types.bool;
       };
     };
   };
+
+  config = mkIf cfg.enable (mkMerge [
+    {
+      nixpkgs.config.allowUnfree = true;
+      time.timeZone = cfg.system.timeZone;
+      system.stateVersion = "20.09";
+      nix.allowedUsers = [ cfg.user.name ];
+
+      networking = mkMerge [
+        {
+          # global useDHCP is deprecated, don't use it
+          useDHCP = false;
+
+          hostName = cfg.networking.hostName;
+        }
+
+        (mkIf (cfg.networking.wiredEthernet != null) {
+          interfaces.${cfg.networking.wiredEthernet}.useDHCP = true;
+        })
+      ];
+
+      boot = {
+        loader = {
+          systemd-boot.enable = true;
+          efi.canTouchEfiVariables = true;
+        };
+        initrd.luks.devices = {
+          decrypted0 = {
+            device = builtins.toString cfg.rootDevice.encryptedDisk;
+            preLVM = true;
+            allowDiscards = cfg.rootDevice.isSolidState;
+          };
+        };
+      };
+
+      i18n.defaultLocale = cfg.system.locale;
+      console.useXkbConfig = true;
+
+      users = {
+        mutableUsers = false;
+
+        groups.${cfg.user.name} = {
+          gid = 1000;
+        };
+
+        users.${cfg.user.name} = {
+          isNormalUser = true;
+          uid = 1000;
+          group = cfg.user.name;
+          extraGroups = [ "wheel" ];
+          hashedPassword = cfg.user.sha256Password;
+          createHome = true;
+          home = "/home/${cfg.user.name}";
+          shell = pkgs.fish;
+        };
+      };
+
+      services.xserver = {
+        enable = true;
+        xkbOptions = "ctrl:nocaps";
+        displayManager.defaultSession = "xfce";
+        desktopManager.xfce = {
+          enable = true;
+          enableXfwm = false;
+        };
+      };
+
+      environment = {
+        systemPackages = with pkgs; [
+          fish
+          git
+          neovim
+        ];
+
+        variables = {
+          EDITOR = "nvim";
+          VISUAL = "nvim";
+        };
+      };
+
+      home-manager = {
+        useUserPackages = true;
+        useGlobalPkgs = true;
+
+        users.${cfg.user.name} = {
+          programs = {
+            firefox = {
+              enable = true;
+            };
+
+            fish = {
+              enable = true;
+            };
+
+            git = {
+              enable = true;
+              userName = cfg.user.realName;
+              userEmail = cfg.user.email;
+            };
+
+            neovim = {
+              enable = true;
+              extraConfig = ''
+                " mandatory to get plugins to generate
+              '';
+              plugins = with pkgs.vimPlugins; [
+                vim-nix
+              ];
+            };
+          };
+
+          xsession = let userCfg = config.home-manager.users.${cfg.user.name}; in {
+            enable = true;
+
+            # There's some spooky action at a distance here.
+            #
+            # The login manager is going to launch the script at `~/.xsession`.
+            #
+            # We want `~/.xsession` to launch `xfce4-session`, which in turn
+            # launches `xfdesktop`, `xmonad`, `xfce4-panel` etc.
+            #
+            # However, we also want to use the home-manager xmonad derivation
+            # instead of writing our own, since it wraps up the installation in
+            # nice configuration options and sets up recompilation when `xmonad.hs`
+            # changes on reconciliation.
+            # https://github.com/nix-community/home-manager/blob/master/modules/services/window-managers/xmonad.nix
+            #
+            # The home-manager xmonad derivation explicitly sets up
+            # `windowManager.command`, which is what is launched by `.xsession`, so
+            # we have to juggle things to get the configuration we want.
+
+            initExtra = let
+              xmonad = util.findPackage userCfg.home.packages "xmonad-with-packages";
+            in ''
+              # We only really need to run this once.
+              xfconf-query -c xfce4-session -p /general/SaveOnExit -s false
+
+              # ${xmonad}/bin/xmonad
+            '';
+
+            # Override (with `mkForce`) to make `~/.xsession` launch xfce4-session
+            # instead of xmonad directly
+            windowManager.command = pkgs.lib.mkForce "${pkgs.runtimeShell} ${pkgs.xfce.xfce4-session.xinitrc}";
+
+            windowManager.xmonad = {
+              enable = true;
+              enableContribAndExtras = true;
+              extraPackages = haskellPkgs: with haskellPkgs; [
+                dbus
+              ];
+              config = ./xmonad.hs;
+            };
+          };
+        };
+      };
+    }
+
+    (mkIf cfg.rootDevice.isSolidState {
+      fileSystems."/".options = [ "noatime" "nodiratime" "discard" ];
+    })
+  ]);
 }
