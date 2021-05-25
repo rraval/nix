d9759a78b29f977ba3be4b98635430c6e488e377
Ronuk Raval <ronuk.raval@gmail.com>
2021-05-25 12:50:30 -0400

    Use openpgp.org as a keyserver

--- default.nix
+++ default.nix
@@ -302,6 +302,10 @@ in {
             gpg = {
               enable = true;
 
+              settings = {
+                keyserver = "hkps://keys.openpgp.org";
+              };
+
               # Use PC/SC to talk to smartcards
               # https://ludovicrousseau.blogspot.com/2019/06/gnupg-and-pcsc-conflicts.html
               scdaemonSettings.disable-ccid = true;
