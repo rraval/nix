c78a22b5823eab7c7e0491c1eae2c6bbb45cc6f4
Ronuk Raval <ronuk.raval@gmail.com>
2022-10-10 12:17:51 -0400

    Update git-nomad to v0.6.0

--- box/packages/git-nomad.nix
+++ box/packages/git-nomad.nix
@@ -1,15 +1,15 @@
 { lib, rustPlatform, fetchFromGitHub }: rustPlatform.buildRustPackage rec {
   pname = "git-nomad";
-  version = "0.5.0";
+  version = "0.6.0";
 
   src = fetchFromGitHub {
     owner = "rraval";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-kcobNP+xEu/nmJplcWNHBnft8/BpwIph6o/JcUiHk60=";
+    sha256 = "sha256-1PXAdXafkPOIVzaWjW/RlWHwYhMqPoj0Hj5JmOMUj8A=";
   };
 
-  cargoHash = "sha256-OY5QHoLxU/e3eVzflQCrXXFRqimB5GKGKAcIYhVcAU4=";
+  cargoHash = "sha256-ULcdJRla1JwI0y6ngW9xQXjNw2wO48HuAczsNIsJJK0=";
 
   meta = with lib; {
     homepage = "https://github.com/rraval/git-nomad";
