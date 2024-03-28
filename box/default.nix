7acc82da510ef6e571d5fb11309bbfa8bb5302e8
Ronuk Raval <ronuk.raval@gmail.com>
2024-03-28 14:39:12 -0400

    Expose port 8888 through the firewall

--- box/default.nix
+++ box/default.nix
@@ -51,9 +51,14 @@ in mkMerge [
 
       networkmanager.enable = true;
 
-      # for VLC Chromecast integration, see
-      # https://github.com/NixOS/nixpkgs/blob/c207be6/pkgs/applications/video/vlc/default.nix#L20
-      firewall.allowedTCPPorts = [ 8010 ];
+      firewall.allowedTCPPorts = [
+        # for VLC Chromecast integration, see
+        # https://github.com/NixOS/nixpkgs/blob/c207be6/pkgs/applications/video/vlc/default.nix#L20
+        8010
+
+        # HTTP servers for local development
+        8888
+      ];
       firewall.allowedUDPPortRanges = [ { from = 32768; to = 60999; } ];
     };
 
