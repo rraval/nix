45171285b15b9a8de6f835c293f55e0978768272
Ronuk Raval <ronuk.raval@gmail.com>
2024-04-06 18:14:12 -0400

    Delete some unused git aliases

--- box/nixos/git.nix
+++ box/nixos/git.nix
@@ -68,10 +68,8 @@
     b = "branch";
     bd = ''!n=$(git symbolic-ref --short HEAD) && git switch --detach && git branch -D "$n"'';
     to = "branch --set-upstream-to";
-    tom = "branch --set-upstream-to origin/master";
     br = "for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:green)%(refname:short)%(color:reset): %(color:red)%(objectname:short)%(color:reset) %(contents:subject) %(color:yellow)(%(authorname) %(committerdate:relative))%(color:reset)'";
     co = "checkout";
-    cob = "!git checkout -b $1 origin/master #";
     cx = "cherry-pick -x";
     fliptable = "!echo '(╯°□°）╯︵ ┻━┻'; git reset --hard HEAD";
     flipup = "!echo '┬─┬ ノ( ゜-゜ノ)'; git reset --hard '@{u}'";
