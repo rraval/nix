ff7187f1cde4c383ffb87aae1f956b02ec0fcba0
Ronuk Raval <ronuk.raval@gmail.com>
2024-05-20 17:55:10 -0400

    Use nightfox for wezterm and fish

--- box/config/wezterm.lua
+++ box/config/wezterm.lua
@@ -1,9 +1,27 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 
-local color_scheme = 'Solarized (dark) (terminal.sexy)'
-local colors = wezterm.color.get_builtin_schemes()[color_scheme]
-colors.scrollbar_thumb = colors.cursor_fg
+-- Simplified version of nightfox theme that skips tab bar stuff.
+-- https://github.com/EdenEast/nightfox.nvim/blob/main/extra/nightfox/wezterm.toml
+local colors = {
+    foreground = "#cdcecf",
+    background = "#192330",
+    cursor_bg = "#cdcecf",
+    cursor_border = "#cdcecf",
+    cursor_fg = "#192330",
+    compose_cursor = '#f4a261',
+    selection_bg = "#2b3b51",
+    selection_fg = "#cdcecf",
+    scrollbar_thumb = "#71839b",
+    split = "#131a24",
+    visual_bell = "#cdcecf",
+    ansi = {"#393b44", "#c94f6d", "#81b29a", "#dbc074", "#719cd6", "#9d79d6", "#63cdcf", "#dfdfe0"},
+    brights = {"#575860", "#d16983", "#8ebaa4", "#e0c989", "#86abdc", "#baa1e2", "#7ad5d6", "#e4e4e5"},
+    indexed = {
+        [16] = "#d67ad2",
+        [17] = "#f4a261",
+    },
+}
 
 return {
     adjust_window_size_when_changing_font_size = false, -- using a tiling window manager
