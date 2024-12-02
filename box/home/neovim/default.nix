9d2088e72e5780b44a8245f83fd8d149d9d7ea9b
Ronuk Raval <ronuk.raval@gmail.com>
2024-12-02 16:49:24 -0500

    neovim: non-directional leap

--- box/home/neovim/default.nix
+++ box/home/neovim/default.nix
@@ -178,7 +178,10 @@
           end
       end
 
-      require("leap").create_default_mappings()
+      vim.keymap.set('n',        's', '<Plug>(leap)')
+      vim.keymap.set('n',        'S', '<Plug>(leap-from-window)')
+      vim.keymap.set({'x', 'o'}, 's', '<Plug>(leap-forward)')
+      vim.keymap.set({'x', 'o'}, 'S', '<Plug>(leap-backward)')
 
       local function leapGetTargets (buf)
         local pick = require('telescope.actions.state').get_current_picker(buf)
