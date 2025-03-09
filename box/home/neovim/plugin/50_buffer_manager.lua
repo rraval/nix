7ba34cc9f7d79c60e0f2b13f44b3e4dbae07757f
Ronuk Raval <ronuk.raval@gmail.com>
2025-03-09 15:26:58 -0400

    nvim: bigger buffer manager

--- box/home/neovim/plugin/50_buffer_manager.lua
+++ box/home/neovim/plugin/50_buffer_manager.lua
@@ -1,4 +1,6 @@
 require("buffer_manager").setup({
+  width = math.floor(vim.o.columns * 0.8),
+  height = math.floor(vim.o.lines * 0.8),
   order_buffers = "fullpath",
   show_indicators = true,
   short_term_names = true,
