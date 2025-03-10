6f93019fc1c942173070ac7195a31faadd737ffd
Ronuk Raval <ronuk.raval@gmail.com>
2025-03-10 18:11:03 -0400

    nvim: default blink keymap preset

--- box/home/neovim/plugin/50_blink.lua
+++ box/home/neovim/plugin/50_blink.lua
@@ -1,13 +1,8 @@
 require("blink.cmp").setup({
-  keymap = { preset = 'enter' },
+  keymap = { preset = 'default' },
   sources = {
     per_filetype = {
       codecompanion = { "codecompanion" },
     }
   },
-  completion = {
-    menu = {
-      auto_show = function(ctx) return ctx.mode ~= 'cmdline' end,
-    },
-  }
 })
