c2ab1fc4897f532b07ce86b426e89fb1fa766232
Ronuk Raval <ronuk.raval@gmail.com>
2025-03-14 17:02:22 -0400

    nvim: blink tweaks

--- box/home/neovim/plugin/50_blink.lua
+++ box/home/neovim/plugin/50_blink.lua
@@ -3,6 +3,40 @@ require("blink.cmp").setup({
   sources = {
     per_filetype = {
       codecompanion = { "codecompanion" },
-    }
+    },
+    providers = {
+      path = {
+        opts = {
+          get_cwd = function()
+            return vim.fn.getcwd()
+          end,
+        },
+      },
+      cmdline = {
+        min_keyword_length = function(ctx)
+          -- when typing a command, only show when the keyword is 3 characters or longer
+          if ctx.mode == 'cmdline' and string.find(ctx.line, ' ') == nil then return 3 end
+          return 0
+        end
+      },
+    },
+  },
+  completion = {
+    list = {
+      selection = {
+        preselect = false,
+      },
+    },
+  },
+  cmdline = {
+    keymap = { preset = 'default' },
+    completion = {
+      menu = { auto_show = true },
+      list = {
+        selection = {
+          preselect = false,
+        },
+      },
+    },
   },
 })
