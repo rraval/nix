bcf0dd4a5eeb9c0afca9288e9459f019b2b4c7b3
Ronuk Raval <ronuk.raval@gmail.com>
2025-09-11 17:17:31 -0400

    disable copilot for .env files

--- box/home/neovim/plugin/50_copilot.lua
+++ box/home/neovim/plugin/50_copilot.lua
@@ -10,4 +10,21 @@ require('copilot').setup({
       dismiss = "<M-h>",
     },
   },
+  should_attach = function (bufnr, bufname)
+    -- retain default behavior
+    if not vim.bo.buflisted then
+      return false
+    end
+
+    if vim.bo.buftype ~= "" then
+      return false
+    end
+
+    -- disable for .env / .envrc etc.
+    if string.match(vim.fs.basename(bufname), '^%.env.*') then
+      return false
+    end
+
+    return true
+  end,
 })
