1730d8a07f5810a0729fdec4593f82b237d92686
Ronuk Raval <ronuk.raval@gmail.com>
2025-11-23 09:50:01 -0500

    move off ssh default config

--- box/home/ssh/default.nix
+++ box/home/ssh/default.nix
@@ -2,10 +2,21 @@
 {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
     matchBlocks = {
       "*" = {
         userKnownHostsFile = "${config.home.homeDirectory}/.ssh/known_hosts ${./known_hosts}";
         serverAliveInterval = 60;
+
+        # Defaults from previous versions of the SSH module.
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
       };
     };
   };
